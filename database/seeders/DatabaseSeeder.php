7a899c35b20a5fc41fac16d2fa27a0f357352623
NurKholisYusufRabbani <nurkholis.23074@mhs.unesa.ac.id>
2026-06-11 04:25:46 +0700

    Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\Store;
 use App\Models\Product;
 use App\Models\Transaction;
+use App\Models\RawMaterial;
 
 class DatabaseSeeder extends Seeder
 {
@@ -27,7 +28,7 @@ class DatabaseSeeder extends Seeder
         );
         $this->command->info('Akun Admin berhasil dibuat! (admin@nusantara.com / password123)');
 
-        $this->command->info('Memuat 5.000 data dari Excel');
+        $this->command->info('Memuat 5.000 data dari Excel...');
 
         $file = storage_path('app/private/Coffee_Shop_Sales_Nusantara_Clean.xlsx');
 
@@ -81,49 +82,171 @@ class DatabaseSeeder extends Seeder
             DB::statement('SET FOREIGN_KEY_CHECKS=1;');
             $this->command->info("Selesai memuat $count data transaksi dari Excel!");
 
-            $this->command->info('Menyiapkan Master Bahan Baku & Resep...');
+            $this->command->info('Menyiapkan data bahan baku untuk setiap kategori produk...');
 
             $rawMaterials = [
-                ['name' => 'Biji Kopi Arabica', 'sku' => 'BB-001', 'unit' => 'Gram', 'price_per_unit' => 150.00, 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'Biji Kopi Robusta', 'sku' => 'BB-002', 'unit' => 'Gram', 'price_per_unit' => 120.00, 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'Susu Fresh Milk', 'sku' => 'BB-003', 'unit' => 'Mililiter', 'price_per_unit' => 25.00, 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'Gula Aren Cair', 'sku' => 'BB-004', 'unit' => 'Mililiter', 'price_per_unit' => 15.00, 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'Cup Plastik + Sedotan', 'sku' => 'BB-005', 'unit' => 'Pcs', 'price_per_unit' => 800.00, 'created_at' => now(), 'updated_at' => now()],
+                ['name' => 'Biji Kopi Espresso Blend', 'sku' => 'BB-001', 'unit' => 'Gram', 'price_per_unit' => 150.00],
+                ['name' => 'Biji Kopi Nusantara Blend', 'sku' => 'BB-002', 'unit' => 'Gram', 'price_per_unit' => 120.00],
+                ['name' => 'Biji Kopi Single Origin', 'sku' => 'BB-003', 'unit' => 'Gram', 'price_per_unit' => 180.00],
+                ['name' => 'Daun Teh Hitam Premium', 'sku' => 'BB-004', 'unit' => 'Gram', 'price_per_unit' => 50.00],
+                ['name' => 'Daun Teh Spesial (Earl Grey/Peppermint)', 'sku' => 'BB-005', 'unit' => 'Gram', 'price_per_unit' => 75.00],
+                ['name' => 'Bubuk Kakao Jawa & Dark Choco', 'sku' => 'BB-006', 'unit' => 'Gram', 'price_per_unit' => 80.00],
+                ['name' => 'Susu Fresh Milk (UHT)', 'sku' => 'BB-007', 'unit' => 'Mililiter', 'price_per_unit' => 25.00],
+                ['name' => 'Gula Aren Cair', 'sku' => 'BB-008', 'unit' => 'Mililiter', 'price_per_unit' => 15.00],
+                ['name' => 'Gula Pasir & Gula Batu', 'sku' => 'BB-009', 'unit' => 'Gram', 'price_per_unit' => 12.00],
+                ['name' => 'Sirup Premium (Vanilla/Karamel/Pandan)', 'sku' => 'BB-010', 'unit' => 'Mililiter', 'price_per_unit' => 30.00],
+                ['name' => 'Rempah Tradisional (Jahe/Serai)', 'sku' => 'BB-011', 'unit' => 'Gram', 'price_per_unit' => 45.00],
+                ['name' => 'Tepung Terigu & Mentega', 'sku' => 'BB-012', 'unit' => 'Gram', 'price_per_unit' => 20.00],
+                ['name' => 'Bahan Isian (Cokelat/Keju/Ayam)', 'sku' => 'BB-013', 'unit' => 'Gram', 'price_per_unit' => 50.00],
+                ['name' => 'Roti Tawar & Singkong Mentah', 'sku' => 'BB-014', 'unit' => 'Porsi', 'price_per_unit' => 3000.00],
+                ['name' => 'Beras & Mie Mentah', 'sku' => 'BB-015', 'unit' => 'Gram', 'price_per_unit' => 15.00],
+                ['name' => 'Daging Ayam & Sapi Segar', 'sku' => 'BB-016', 'unit' => 'Gram', 'price_per_unit' => 120.00],
+                ['name' => 'Bumbu Dapur & Sayuran', 'sku' => 'BB-017', 'unit' => 'Gram', 'price_per_unit' => 10.00],
             ];
-            DB::table('raw_materials')->insert($rawMaterials);
-            $materialIds = DB::table('raw_materials')->pluck('id')->toArray();
+
+            $materialMap = [];
+            foreach ($rawMaterials as $rm) {
+                $inserted = RawMaterial::firstOrCreate(
+                    ['sku' => $rm['sku']],
+                    [
+                        'name' => $rm['name'], 
+                        'unit' => $rm['unit'], 
+                        'price_per_unit' => $rm['price_per_unit']
+                    ]
+                );
+                $materialMap[$rm['name']] = $inserted->id;
+            }
 
             $this->command->info('Mendistribusikan stok gudang ke tiap cabang...');
             $stores = Store::all();
             foreach ($stores as $store) {
-                foreach ($materialIds as $mId) {
-                    DB::table('raw_material_store')->insert([
-                        'store_id'        => $store->id,
-                        'raw_material_id' => $mId,
-                        'current_stock'   => rand(1000, 5000),
-                        'minimum_stock'   => 500,
-                        'created_at'      => now(),
-                        'updated_at'      => now(),
-                    ]);
+                foreach ($materialMap as $name => $mId) {
+                    DB::table('raw_material_store')->updateOrInsert(
+                        ['store_id' => $store->id, 'raw_material_id' => $mId],
+                        [
+                            'current_stock'   => rand(2000, 8000),
+                            'minimum_stock'   => 500,
+                            'created_at'      => now(),
+                            'updated_at'      => now(),
+                        ]
+                    );
                 }
             }
 
-            $this->command->info('Menyusun takaran buku resep untuk tiap menu...');
+            $this->command->info('Menyusun resep menu berdasarkan kategori produk...');
             $products = Product::all();
+            
             foreach ($products as $product) {
-                $usedMaterials = (array) array_rand(array_flip($materialIds), rand(2, 3));
-                foreach ($usedMaterials as $mId) {
-                    DB::table('product_raw_material')->insert([
-                        'product_id'      => $product->id,
-                        'raw_material_id' => $mId,
-                        'qty_needed'      => rand(10, 150),
-                        'created_at'      => now(),
-                        'updated_at'      => now(),
-                    ]);
+                $attachData = [];
+                $namaMenu = strtolower($product->name);
+
+                switch ($product->type) {
+                    case 'Barista Espresso':
+                        $attachData[$materialMap['Biji Kopi Espresso Blend']] = ['qty_needed' => 18];
+                        if (str_contains($namaMenu, 'latte') || str_contains($namaMenu, 'cappuccino')) {
+                            $attachData[$materialMap['Susu Fresh Milk (UHT)']] = ['qty_needed' => 150];
+                        }
+                        break;
+                    
+                    case 'Kopi Lokal Nusantara':
+                        $attachData[$materialMap['Biji Kopi Nusantara Blend']] = ['qty_needed' => 20];
+                        if (str_contains($namaMenu, 'aren')) {
+                            $attachData[$materialMap['Gula Aren Cair']] = ['qty_needed' => 30];
+                        } else {
+                            $attachData[$materialMap['Gula Pasir & Gula Batu']] = ['qty_needed' => 15];
+                        }
+                        // Deteksi kalau ada unsur susu atau sanger
+                        if (str_contains($namaMenu, 'susu') || str_contains($namaMenu, 'sanger') || str_contains($namaMenu, 'tarik')) {
+                            $attachData[$materialMap['Susu Fresh Milk (UHT)']] = ['qty_needed' => 80];
+                        }
+                        break;
+
+                    case 'Single Origin Indonesia':
+                        $attachData[$materialMap['Biji Kopi Single Origin']] = ['qty_needed' => 20];
+                        break;
+
+                    case 'Teh Nusantara & Klasik':
+                        if (str_contains($namaMenu, 'earl grey') || str_contains($namaMenu, 'peppermint')) {
+                            $attachData[$materialMap['Daun Teh Spesial (Earl Grey/Peppermint)']] = ['qty_needed' => 10];
+                        } elseif (str_contains($namaMenu, 'lemon')) {
+                            $attachData[$materialMap['Daun Teh Hitam Premium']] = ['qty_needed' => 10];
+                            $attachData[$materialMap['Rempah Tradisional (Jahe/Serai)']] = ['qty_needed' => 15];
+                        } else {
+                            $attachData[$materialMap['Daun Teh Hitam Premium']] = ['qty_needed' => 15];
+                        }
+                        
+                        // Tambah gula/susu sesuai nama
+                        if (!str_contains($namaMenu, 'tawar')) {
+                            $attachData[$materialMap['Gula Pasir & Gula Batu']] = ['qty_needed' => 20];
+                        }
+                        if (str_contains($namaMenu, 'tarik')) {
+                            $attachData[$materialMap['Susu Fresh Milk (UHT)']] = ['qty_needed' => 100];
+                        }
+                        break;
+                    
+                    case 'Hot chocolate':
+                        $attachData[$materialMap['Bubuk Kakao Jawa & Dark Choco']] = ['qty_needed' => 30];
+                        $attachData[$materialMap['Susu Fresh Milk (UHT)']] = ['qty_needed' => 150];
+                        $attachData[$materialMap['Gula Pasir & Gula Batu']] = ['qty_needed' => 10];
+                        break;
+
+                    case 'Wedang & Jamu':
+                        $attachData[$materialMap['Rempah Tradisional (Jahe/Serai)']] = ['qty_needed' => 25];
+                        $attachData[$materialMap['Gula Pasir & Gula Batu']] = ['qty_needed' => 20];
+                        if (str_contains($namaMenu, 'bajigur')) {
+                            $attachData[$materialMap['Susu Fresh Milk (UHT)']] = ['qty_needed' => 50]; // Anggap santan pakai stok susu sementara
+                        }
+                        break;
+
+                    case 'Regular syrup':
+                        $attachData[$materialMap['Sirup Premium (Vanilla/Karamel/Pandan)']] = ['qty_needed' => 15];
+                        $attachData[$materialMap['Gula Aren Cair']] = ['qty_needed' => 10];
+                        break;
+
+                    case 'Jajanan Pasar':
+                    case 'Pastry':
+                        $attachData[$materialMap['Tepung Terigu & Mentega']] = ['qty_needed' => 80];
+                        $attachData[$materialMap['Bahan Isian (Cokelat/Keju/Ayam)']] = ['qty_needed' => 30];
+                        break;
+                    
+                    case 'Roti & Gorengan':
+                        $attachData[$materialMap['Roti Tawar & Singkong Mentah']] = ['qty_needed' => 1];
+                        if (str_contains($namaMenu, 'coklat') || str_contains($namaMenu, 'keju')) {
+                            $attachData[$materialMap['Bahan Isian (Cokelat/Keju/Ayam)']] = ['qty_needed' => 25];
+                        } else {
+                            $attachData[$materialMap['Tepung Terigu & Mentega']] = ['qty_needed' => 20];
+                        }
+                        break;
+
+                    case 'Berkuah':
+                        $attachData[$materialMap['Daging Ayam & Sapi Segar']] = ['qty_needed' => 100];
+                        $attachData[$materialMap['Bumbu Dapur & Sayuran']] = ['qty_needed' => 50];
+                        break;
+
+                    case 'Nasi & Mie':
+                        $attachData[$materialMap['Beras & Mie Mentah']] = ['qty_needed' => 150];
+                        $attachData[$materialMap['Daging Ayam & Sapi Segar']] = ['qty_needed' => 60];
+                        $attachData[$materialMap['Bumbu Dapur & Sayuran']] = ['qty_needed' => 30];
+                        break;
+
+                    default:
+                        $attachData[$materialMap['Bumbu Dapur & Sayuran']] = ['qty_needed' => 20];
+                        break;
+                }
+
+                foreach ($attachData as $mId => $pivotData) {
+                    DB::table('product_raw_material')->updateOrInsert(
+                        ['product_id' => $product->id, 'raw_material_id' => $mId],
+                        [
+                            'qty_needed' => $pivotData['qty_needed'],
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                        ]
+                    );
                 }
             }
 
-            $this->command->info('Seed data bahan baku dan resep berhasil disiapkan!');
+            $this->command->info('Seed data berhasil dimuat dan logika bahan baku & resep menu selesai dijalankan!');
 
         } else {
             $this->command->error(SimpleXLSX::parseError());
